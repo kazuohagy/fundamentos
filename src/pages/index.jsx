33013e1c11ee0a748b1330a465151ffbaa7ee4bb
kazuo <kazuohagy@users.noreply.github.com>
2023-08-14 00:36:02 -0300

    feito jsx componente react next basta criar no pages exportando a funcao

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,8 @@
 export default function inicio() {
-    return "mario"
+    return (
+        <div className="">
+            <h1>Fundamentos NextJS</h1>
+            <h2>Vamos estudar essse framework</h2>
+        </div>
+    )
 }
