7782cb7f726930d9e7b43eeeb96d6ff2dce26373
kazuo <kazuohagy@users.noreply.github.com>
2023-08-15 14:08:44 -0300

    retornando mais de um elemento

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -2,6 +2,10 @@ import Cabecalho from "../components/Cabecalho";
 
 export default function Exemplo() {
     return (
+        <div>
         <Cabecalho />
+        <Cabecalho />
+
+        </div>
     )
 }
