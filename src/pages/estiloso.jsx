ded99d5af35328b0d45c054d646b98f76bf0fc72
kazuo <kazuohagy@users.noreply.github.com>
2023-08-16 09:28:30 -0300

    navegacao NextJs

--- src/pages/estiloso.jsx
+++ src/pages/estiloso.jsx
@@ -1,7 +1,9 @@
 import styles from './Estiloso.module.css'
+import Link from 'next/link'
 export default function Estiloso(){
     return (
         <div className={styles.roxo}>
+            <Link href="/">Voltar</Link>
             <h1>Estiloso</h1>
             <h2>Exemplo de CSS</h2>
         </div>
