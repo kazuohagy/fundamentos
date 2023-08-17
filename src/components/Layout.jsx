2c0c4e800b7bc9c2582a4ded5faeb203bff804f7
kazuo <kazuohagy@users.noreply.github.com>
2023-08-17 10:09:06 -0300

    preciso trabalhar agora

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,12 @@
+import Link from "next/link";
+import styles from "../styles/Layout.module.css";
 export default function Layout(props) {
     return (
         <div className={styles.layout}>
+            
             <div className={styles.cabecalho}>
-                <h1>Header</h1>
+                <h1>{props.titulo}</h1>
+                <Link href="/">Voltar</Link>
             </div>
             <div className={styles.conteudo}>
                 {props.children}
