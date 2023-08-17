76847ca0139061e4ae868fd1d3beff7254bbce5f
kazuo <kazuohagy@users.noreply.github.com>
2023-08-17 13:32:22 -0300

    index.jsx alterado style

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,11 @@
 import Link from 'next/link'
+import Navegador from '../components/Navegador'
 export default function inicio() {
     return (
-        <div className="">
-            <Link href="/">Inicio</Link><br></br>
-            <Link href="/estiloso">Estiloso</Link><br />
-            <Link href="/app">App</Link><br />
+        <div className="" style={{display:'flex', justifyContent:'center', alignItems:'center',height:'100vh', flexWrap:'wrap'}}>
+            <Navegador destino="/" texto="Inicio" cor="#9400d3" />
+            <Navegador destino="/estiloso" texto="Estiloso"  />
+            <Navegador destino="/app" texto="App" cor="#9400d3" />
         </div>
     )
 }
