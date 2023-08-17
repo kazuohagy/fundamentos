a0f7e2d25c42e1d7cad6347167db0a05f69dd9cb
kazuo <kazuohagy@users.noreply.github.com>
2023-08-17 13:31:56 -0300

    app.jsx add layout

--- src/pages/app.jsx
+++ src/pages/app.jsx
@@ -1,11 +1,14 @@
 import Cabecalho from "../components/Cabecalho";
+import Layout from "../components/Layout";
 
 export default function Exemplo() {
     return (
+        <Layout titulo="Usando Componentes">
         <div>
         <Cabecalho titulo="Mario" />
         <Cabecalho titulo="Luigi" />
 
         </div>
+        </Layout>
     )
 }
