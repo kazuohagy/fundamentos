01c15a9cb4b385cd4cef0d9bf300a6e051358d04
kazuo <kazuohagy@users.noreply.github.com>
2023-08-18 05:22:37 -0300

    boa noite

---
+++ src/pages/navegacao/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "@/src/components/Layout";
+
+export default function Navegacao(){
+    return (
+        <Layout titulo="Navegação Dinâmica">
+            <h1>Navegação Dinâmica</h1>
+        </Layout>
+    )
+}
