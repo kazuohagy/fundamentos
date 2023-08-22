e600710ed10b42bef76feac122aa0443d16912b6
kazuo <kazuohagy@users.noreply.github.com>
2023-08-22 10:07:36 -0300

    :D

--- src/pages/navegacao/index.jsx
+++ src/pages/navegacao/index.jsx
@@ -3,7 +3,7 @@ import Layout from "@/src/components/Layout";
 export default function Navegacao(){
     return (
         <Layout titulo="Navegação Dinâmica">
-            <h1>Navegação Dinâmica</h1>
+            <h1>Navegação Dinâmica #01</h1>
         </Layout>
     )
 }
